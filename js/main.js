44c7217faeed577b66924f9879bec5b5d40edd38
Tobias <Windows@Tobias>
2023-03-07 21:11:35 -0300

    projecto finalizado, sin imagenes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let menu = document.querySelector('.menu-icon');
-
+let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
+    navbar.classList.toggle("open-menu")
     menu.classList.toggle("move");
 };
-
+window.onscroll = () =>{
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move");
+}
 // Review Swiper
 
 var swiper = new Swiper(".reviews-content", {
@@ -62,3 +66,17 @@ var swiper = new Swiper(".reviews-content", {
         icon: "success",
       });
   }
+
+  //Header Background Change On Scroll
+
+let header = document.querySelector("header");
+
+window.addEventListener('scroll',() =>{
+  header.classList.toggle('header-active',window.scrollY > 0)
+})
+//scroll top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll",() =>{
+    scrollTop.classList.toggle("scroll-active",window.scrollY > 0); 
+});
